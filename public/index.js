77573b276685dbed238425c0af72ad227ec8fda4
Marshall Lorhan <mlorhan@gmail.com>
2024-01-30 21:31:27 -0800

    added fetch

--- public/index.js
+++ public/index.js
@@ -1,7 +1,11 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+
+    const response = await fetch("http://http://127.0.0.1:9001/counter");
+    const main = await response.json();
+    console.log(main)
 
     let countValue = 0;
 
